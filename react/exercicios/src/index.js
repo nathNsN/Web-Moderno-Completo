97b068ee248dcf1adb5186d0abb2812353bda03b
nathNsN <nathansantos3211@gmail.com>
2022-07-25 23:15:05 -0300

    Segunda bloco de aulas - componentes

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,12 +3,21 @@ import ReactDom from 'react-dom';
 
 // import Multiplos from './componentes/Multiplos'
 // import {BoaTarde, BoaNoite} from './componentes/Multiplos'
-import Saudacao from './componentes/Saudacao'
+// import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDom.render(
     <div>
-        <Saudacao tipo="Bom dia" nome="Nathan" />
-        {/* <Multiplos.BoaTarde nome='Ana'/>
+        <Pai nome="Paulo" sobrenome="Silva">
+            <Filho nome="Pedro" />
+            <Filho nome="Paulo"/>
+            <Filho nome="Carla"/>
+        </Pai>
+
+
+        {/* <Saudacao tipo="Bom dia" nome="Nathan" />
+        <Multiplos.BoaTarde nome='Ana'/>
         <Multiplos.BoaNoite nome='Ana'/> */}
     </div>
     , document.getElementById('root'));
